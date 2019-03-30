87f05a908eba473299dce5ef6c95def0d0c5dbc5
chenShengYa <13867726371@163.com>
2019-03-30 22:21:39 +0800

    :tada:class

---
+++ typescript/practice/class/demo1.js
@@ -0,0 +1,13 @@
+"use strict";
+var Greeter = /** @class */ (function () {
+    function Greeter(message) {
+        this.greeting = message;
+    }
+    Greeter.prototype.greet = function () {
+        return "Hello, " + this.greeting;
+    };
+    return Greeter;
+}());
+var a;
+a = new Greeter("world");
+console.log(a.greet());
